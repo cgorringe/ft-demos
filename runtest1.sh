fd555266fa53352c0c538cf5387962593c4c93db
Carl Gorringe <git@gorringe.org>
2016-05-18 22:32:36 -0700

    test

--- runtest1.sh
+++ runtest1.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
 set -e
+G=-g25x20
 
 while :; do
-./plasma  -g25x20 -t5 &
-./nb-logo -g25x20 -t5
-../client/send-text -g25x20 -f fonts/5x8.bdf -o -l6 Welcome to Noisebridge &
-./blur    -g25x20 -t5 bolt
-./blur    -g25x20 -t5 boxes
-./fractal -g25x20 -t5
+./plasma  $G -t5 &
+./nb-logo $G -t5
+../client/send-text $G -f fonts/5x8.bdf -o -l6 Welcome to Noisebridge &
+./blur    $G -t5 bolt
+./blur    $G -t5 boxes
+./fractal $G -t5
+./hack    $G -r1 hack
+./lines   $G -t5 two
+./quilt   $G -t5
 done
