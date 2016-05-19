a5db25da136abfca60b4587fbb59596f36cd0ee0
Carl Gorringe <git@gorringe.org>
2016-05-18 21:47:21 -0700

    options for quilt

--- quilt.cc
+++ quilt.cc
@@ -9,11 +9,14 @@
 //
 // How to run:
 //
+// To see command line options:
+//  ./quilt -?
+//
 // By default, connects to the installation at Noisebridge. If using a
 // different display (e.g. a local terminal display)
 // pass the hostname as parameter:
 //
-//  ./quilt localhost
+//  ./quilt -h localhost
 //
 // or set the environment variable FT_DISPLAY to not worry about it
 //
@@ -37,18 +40,97 @@
 
 #include "udp-flaschen-taschen.h"
 
+#include <getopt.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string>
 
 //                               large  small
 #define DISPLAY_WIDTH  (9*5)  //  9*5    5*5
 #define DISPLAY_HEIGHT (7*5)  //  7*5    4*5
-#define Z_LAYER 15      // (0-15) 0=background
+#define Z_LAYER 2      // (0-15) 0=background
 #define DELAY 10  // 200
 #define SKIP_NUM 5            // width or height of crate
 
+
+volatile bool interrupt_received = false;
+static void InterruptHandler(int signo) {
+    interrupt_received = true;
+}
+
+// ------------------------------------------------------------------------------------------
+// Command Line Options
+
+// option vars
+const char *opt_hostname = NULL;
+int opt_layer  = Z_LAYER;
+double opt_timeout = 60*60*24;  // timeout in 24 hrs
+int opt_width  = DISPLAY_WIDTH;
+int opt_height = DISPLAY_HEIGHT;
+int opt_xoff=0, opt_yoff=0;
+int opt_delay   = DELAY;
+
+int usage(const char *progname) {
+
+    fprintf(stderr, "Quilt (c) 2016 Carl Gorringe (carl.gorringe.org)\n");
+    fprintf(stderr, "Usage: %s [options]\n", progname);
+    fprintf(stderr, "Options:\n"
+        "\t-g <W>x<H>[+<X>+<Y>] : Output geometry. (default 45x35+0+0)\n"
+        "\t-l <layer>     : Layer 0-15. (default 1)\n"
+        "\t-t <timeout>   : Timeout exits after given seconds. (default 24hrs)\n"
+        "\t-h <host>      : Flaschen-Taschen display hostname. (FT_DISPLAY)\n"
+        "\t-d <delay>     : Delay between frames in milliseconds. (default 10)\n"
+    );
+    return 1;
+}
+
+int cmdLine(int argc, char *argv[]) {
+
+    // command line options
+    int opt;
+    while ((opt = getopt(argc, argv, "?g:l:t:h:d:")) != -1) {
+        switch (opt) {
+        case '?':  // help
+            return usage(argv[0]);
+            break;
+        case 'g':  // geometry
+            if (sscanf(optarg, "%dx%d%d%d", &opt_width, &opt_height, &opt_xoff, &opt_yoff) < 2) {
+                fprintf(stderr, "Invalid size '%s'\n", optarg);
+                return usage(argv[0]);
+            }
+            break;
+        case 'l':  // layer
+            if (sscanf(optarg, "%d", &opt_layer) != 1 || opt_layer < 0 || opt_layer >= 16) {
+                fprintf(stderr, "Invalid layer '%s'\n", optarg);
+                return usage(argv[0]);
+            }
+            break;
+        case 't':  // timeout
+            if (sscanf(optarg, "%lf", &opt_timeout) != 1 || opt_timeout < 0) {
+                fprintf(stderr, "Invalid timeout '%s'\n", optarg);
+                return usage(argv[0]);
+            }
+            break;
+        case 'h':  // hostname
+            opt_hostname = strdup(optarg); // leaking. Ignore.
+            break;
+        case 'd':  // delay
+            if (sscanf(optarg, "%d", &opt_delay) != 1 || opt_delay < 1) {
+                fprintf(stderr, "Invalid delay '%s'\n", optarg);
+                return usage(argv[0]);
+            }
+            break;
+        default:
+            return usage(argv[0]);
+        }
+    }
+
+    return 0;
+}
+
+// ------------------------------------------------------------------------------------------
 
 // random int in range min to max inclusive
 int randomInt(int min, int max) {
@@ -56,23 +138,26 @@ int randomInt(int min, int max) {
 }
 
 int main(int argc, char *argv[]) {
-    const char *hostname = NULL;   // Will use default if not set otherwise.
-    if (argc > 1) {
-        hostname = argv[1];        // Hostname can be supplied as first arg
-    }
+
+    // parse command line
+    if (int e = cmdLine(argc, argv)) { return e; }
 
     srandom(time(NULL)); // seed the random generator
 
     // Open socket and create our canvas.
-    const int socket = OpenFlaschenTaschenSocket(hostname);
-    UDPFlaschenTaschen canvas(socket, DISPLAY_WIDTH, DISPLAY_HEIGHT);
-
-    // const Color red(255, 0, 0);
-    // canvas.SetPixel(0, 0, red);
+    const int socket = OpenFlaschenTaschenSocket(opt_hostname);
+    UDPFlaschenTaschen canvas(socket, opt_width, opt_height);
 
     canvas.Clear();
 
-    while (1) {
+    // handle break
+    signal(SIGTERM, InterruptHandler);
+    signal(SIGINT, InterruptHandler);
+
+    time_t starttime = time(NULL);
+    bool quit = false;
+
+    do {
         int r = randomInt(0, 255);
         int g = randomInt(0, 255);
         int b = randomInt(0, 255);
@@ -80,31 +165,36 @@ int main(int argc, char *argv[]) {
         int x1 = randomInt(0, SKIP_NUM - 1);
         int y1 = randomInt(0, SKIP_NUM - 1);
 
-        for (int y=y1; y < DISPLAY_HEIGHT; y += SKIP_NUM) {
-            for (int x=x1; x < DISPLAY_WIDTH; x += SKIP_NUM) {
+        for (int y=y1; y < opt_height; y += SKIP_NUM) {
+            for (int x=x1; x < opt_width; x += SKIP_NUM) {
 
                 canvas.SetPixel(x, y, Color(r, g, b));
-                canvas.SetPixel(DISPLAY_WIDTH - x, y, Color(r, g, b));
-                canvas.SetPixel(x, DISPLAY_HEIGHT - y, Color(r, g, b));
-                canvas.SetPixel(DISPLAY_WIDTH - x, DISPLAY_HEIGHT - y, Color(r, g, b));
+                canvas.SetPixel(opt_width - x, y, Color(r, g, b));
+                canvas.SetPixel(x, opt_height - y, Color(r, g, b));
+                canvas.SetPixel(opt_width - x, opt_height - y, Color(r, g, b));
 
                 // TEST: width and height may not be equal
                 canvas.SetPixel(y, x, Color(r, g, b));
-                canvas.SetPixel(DISPLAY_WIDTH - y, x, Color(r, g, b));
-                canvas.SetPixel(y, DISPLAY_HEIGHT - x, Color(r, g, b));
-                canvas.SetPixel(DISPLAY_WIDTH - y, DISPLAY_HEIGHT - x, Color(r, g, b));
+                canvas.SetPixel(opt_width - y, x, Color(r, g, b));
+                canvas.SetPixel(y, opt_height - x, Color(r, g, b));
+                canvas.SetPixel(opt_width - y, opt_height - x, Color(r, g, b));
 
                 // send canvas
-                canvas.SetOffset(0, 0, Z_LAYER);
+                canvas.SetOffset(opt_xoff, opt_yoff, opt_layer);
                 canvas.Send();
-                usleep(DELAY * 1000);
+
+                if ( (difftime(time(NULL), starttime) >= opt_timeout) || interrupt_received ) {
+                    quit = true;
+                    break;
+                }
+                usleep(opt_delay * 1000);
             }
+            if (quit) break;
         }
-        /*
-        // send canvas
-        canvas.SetOffset(0, 0, Z_LAYER);
-        canvas.Send();
-        usleep(DELAY * 1000);
-        //*/
-    }
+    } while (!quit);
+
+    // clear canvas on exit
+    canvas.Clear();
+    canvas.Send();
+
 }
