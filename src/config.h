362997db149224649a45be75dd6addb32bb324ff
Carl Gorringe <git@gorringe.org>
2022-10-22 17:40:01 -0700

    update default config for 40x30bb display

--- src/config.h
+++ src/config.h
@@ -11,8 +11,9 @@
 #define CONFIG_H
 
 // Defaults
-#define DISPLAY_WIDTH  (9*5)
-#define DISPLAY_HEIGHT (7*5)  // use 7*5 for original flaschen-taschen
-
+#define DISPLAY_WIDTH  (8*5)  // 9*5
+#define DISPLAY_HEIGHT (6*5)  // use 7*5 for original flaschen-taschen
+#define DISPLAY_XOFF 0
+#define DISPLAY_YOFF 5
 
 #endif  // CONFIG_H
